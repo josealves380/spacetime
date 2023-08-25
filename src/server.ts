a583eab06f64237ba1bdc3e494aca0d99feaf870
Jose <netopereira464@gmail.com>
2023-08-25 20:37:15 -0300

    Create database with prisma

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
-
-app.get('/hello', ()=>{
-  return 'Hello World'
+const prisma = new PrismaClient()
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+  return users
 })
 
-app.listen({
-  port: 3333,
-}).then(()=> {
-  console.log('😉 HTTP server running on http://localhost:3333')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('😉 HTTP server running on http://localhost:3333')
+  })
